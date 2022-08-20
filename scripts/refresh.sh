1117ed5ef9db39569f78839cd675fa51577073e5
Jackboxx <gschwantnermoritz@gmail.com>
2022-08-20 15:40:58 +0200

    config files changed

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 FILES=$(cat $PATH_TO_CONFIG_FILES)
 DIRECTORIES=$(cat $PATH_TO_CONFIG_DIRECTORIES)
+CURRENT=$(pwd)
+
+cd $PATH_TO_CONFIG 
 
 for file in $FILES
 do
-   cp $HOME/$file $PATH_TO_CONFIG 
+   cp $HOME/$file .
 done
 
 for directory in $DIRECTORIES
 do
-    cp -r $HOME/$directory $PATH_TO_CONFIG 
+    cp -r $HOME/$directory . 
 done
+
+UPDATE=$(git diff | wc -l)
+
+if [ $UPDATE -gt 0 ]
+then
+    git status 
+    read -p "Push Changes [y/n]" CONTINUE 
+    if [ $CONTINUE = "y" ]
+    then 
+        git add . && git commit -m 'config files changed' && git push
+    fi
+fi
