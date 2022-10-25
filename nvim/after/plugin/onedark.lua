8066cebc6ba242af68518b796ae91b303f65c922
Jackboxx <gschwantnermoritz@gmail.com>
2022-10-25 08:48:27 +0200

    add post 0.8 nvim support to onedark

--- nvim/after/plugin/onedark.lua
+++ nvim/after/plugin/onedark.lua
@@ -4,6 +4,7 @@ require('onedark').setup {
       red = '#88fcf8',
   },
   highlights = {
+    -- < 0.8
     TSKeyword = {fg = '#31eae4', fmt =  'italic' },
     TSKeywordFunction = {fg = '#31eae4', fmt =  'italic' },
     TSKeywordOperator =  {fg = '#31eae4', fmt =  'italic' },
@@ -11,6 +12,14 @@ require('onedark').setup {
     TSVariable = {fg = '#e3eadc'},
     TSVariableBuiltin = {fg = '#e3eadc', fmt = 'underline'},
     TSComment = {fg = '#4d4d4f'},
+    -- > 0.8
+    ["@keyword"] = {fg = '#31eae4', fmt =  'italic' },
+    ["@keyword.function"] = {fg = '#31eae4', fmt =  'italic' },
+    ["@keyword.operator"] =  {fg = '#31eae4', fmt =  'italic' },
+    ["@include"] =  {fg = '#31eae4', fmt =  'bold' },
+    ["@variable"] = {fg = '#e3eadc'},
+    ["@variable.builtin"] = {fg = '#e3eadc', fmt = 'underline'},
+    ["@comment"] = {fg = '#4d4d4f'},
   }
 }
 
