3add77d927c4f13e686d474494bcb3e3ae769ed8
Jackboxx <gschwantnermoritz@gmail.com>
2022-12-10 23:34:09 +0100

    update dev

--- scripts/setup/dev.sh
+++ scripts/setup/dev.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 
+sudo pacman -S cargo
+
+cargo install exa
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions \
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
