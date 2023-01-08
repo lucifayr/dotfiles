52d9c0efcf6ef6087160ee66b2618fc44ab2613e
jackboxx <lucygschwantner@gmail.com>
2023-01-08 15:41:47 +0100

    RAGE

--- scripts/setup/strap.sh
+++ scripts/setup/strap.sh
@@ -2,6 +2,6 @@
 
 pacstrap /mnt base linux linux-firmware grub efibootmgr \ 
     man-db base-devel sudo networkmanager xclip xdg-utils git github-cli fzf \
-    neovim zsh curl
+    neovim i3-gaps zsh curl
 
 echo "Don't forget to install the needed GPU drivers"
