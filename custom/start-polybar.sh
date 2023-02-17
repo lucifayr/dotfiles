9822d046942094ebc50bdde1987b4e6e9dea7dba
Jackboxx <gschwantnermoritz@gmail.com>
2023-02-17 23:24:45 +0100

    update polybar for small displays

--- custom/start-polybar.sh
+++ custom/start-polybar.sh
@@ -1,8 +1,11 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    if [ $m = "DP-0" ] || [ $m = "eDP-1" ]
+    if [ $m = "DP-0" ]
     then
         MONITOR=$m polybar -c ~/.config/polybar/config.ini &
+    elif [ $m = "eDP-1" ]
+    then 
+        MONITOR=$m polybar -c ~/.config/polybar/config-small.ini &
     elif [ $m = "DP-2" ]
     then 
         MONITOR=$m polybar -c ~/.config/polybar/config-left.ini &
