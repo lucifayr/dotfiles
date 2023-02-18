0853b72c2f19372b088e806f92d148d8262a1bba
Jackboxx <gschwantnermoritz@gmail.com>
2023-02-18 12:15:24 +0100

    revert mistake

--- nvim/after/plugin/diagnostics.lua
+++ nvim/after/plugin/diagnostics.lua
@@ -1,3 +1,4 @@
+vim.api.nvim_set_keymap('n', '<leader>es', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>pe', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ne', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>tes', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
