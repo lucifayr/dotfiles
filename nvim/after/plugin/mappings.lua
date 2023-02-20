d595a4a7e18112ec6bbfe77807256e9ec5cdd63a
jackboxx <lucygschwantner@gmail.com>
2023-02-20 12:41:02 +0100

    better shifting

--- nvim/after/plugin/mappings.lua
+++ nvim/after/plugin/mappings.lua
@@ -1,10 +1,6 @@
 vim.api.nvim_set_keymap('n', '<leader>a', 'za', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>A', '<cmd>:set foldlevel=0<cr>', { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', 'J', ':m+<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'K', ':m-2<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', 'U', ':redo<cr>', { noremap = true, silent = true })
-
 vim.api.nvim_set_keymap('n', '<leader>pv', ':NERDTreeFind<cr>:wincmd l<cr>:wincmd q<cr>', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>ga', '<cmd>:Git add %<cr>', { noremap = true, silent = true })
@@ -18,3 +14,11 @@ vim.api.nvim_set_keymap('n', '<leader>sc', ':terminal gramma check %<cr>', { nor
 vim.api.nvim_set_keymap('n', '<leader>W', ':Neorg workspace', { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('x', '<leader>p', '\"_dP', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', 'K', ':m \'<-2<cr>gv=gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'J', ':m \'>+1<cr>gv=gv', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', 'U', ':redo<cr>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('x', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
