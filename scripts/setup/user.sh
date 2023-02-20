7cc571be174f5ac2f495eb9671e05f9cc8ed6a6f
Jackboxx <57196053+Jackboxx@users.noreply.github.com>
2023-02-20 23:26:09 +0100

    Update user.sh

--- scripts/setup/user.sh
+++ scripts/setup/user.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "remember to uncomment the wheel group in /etc/sudoers!"
+
 useradd -m -G wheel -s /bin/zsh jackboxx 
 passwd jackboxx
-echo "remember to uncomment the wheel group in /etc/sudoers!"
