0026e677b30ae5d01f3f54e72f3972cbc9cf0518
Jackboxx <57196053+Jackboxx@users.noreply.github.com>
2023-02-20 21:08:23 +0100

    Update strap.sh

--- scripts/setup/strap.sh
+++ scripts/setup/strap.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-pacstrap /mnt base linux linux-firmware grub efibootmgr man-db \ 
-    man-db base-devel sudo networkmanager uthash git github-cli fzf \
-    neovim zsh curl
+pacstrap /mnt base linux linux-firmware grub efibootmgr man-db base-devel sudo networkmanager uthash git github-cli fzf neovim zsh curl
 
 echo "Don't forget to install the needed GPU drivers"
