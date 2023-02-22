e433b6ea62544003022f3de5609f1e5b4d165c2f
Jackboxx <lucygschwantner@gmail.com>
2023-02-22 23:03:45 +0100

    add flatpak dependency

--- scripts/setup/dev.sh
+++ scripts/setup/dev.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-sudo pacman -S cargo python python-pip go nodejs-lts-hydrogen npm gopls rust-analyzer meson ninja thefuck
+sudo pacman -S cargo python python-pip go nodejs-lts-hydrogen npm gopls rust-analyzer meson ninja thefuck flatpak
 
 sudo npm i -g svelte-language-server typescript-language-server typescript
 
 cargo install exa
-pip3 install --user pywal
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
