1cf943a941ff2cbd8c9e96f078a3e0075cee3e3c
jackboxx <lucygschwantner@gmail.com>
2023-02-24 00:11:41 +0100

    Update style.sh

--- scripts/setup/style.sh
+++ scripts/setup/style.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo pacman -S python-pywal neofetch
 
-cd /tmp && git clone https://github.com/pipeseroni/pipes.sh && cd pipes && sudo make install
+cd /tmp && git clone https://github.com/pipeseroni/pipes.sh && cd pipes.sh && sudo make install
