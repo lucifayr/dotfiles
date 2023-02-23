b90efc662a6dc771a24a9b9c9b94a2494870a4f4
jackboxx <lucygschwantner@gmail.com>
2023-02-23 23:33:51 +0100

    Add lua lsp

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -5,12 +5,24 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local lspconfig = require('lspconfig')
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'rust_analyzer', 'pyright', 'tsserver', 'gopls', 'html', 'cssls', 'marksman', 'astro', 'svelte', 'texlab'}
+local servers = { 
+    'rust_analyzer',
+    'pyright',
+    'tsserver',
+    'gopls',
+    'html',
+    'cssls',
+    'marksman',
+    'astro',
+    'lua_ls',
+    'svelte',
+    'texlab'
+}
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
+    lspconfig[lsp].setup {
+        -- on_attach = my_custom_on_attach,
+        capabilities = capabilities,
+    }
 end
 
 local luasnip = require 'luasnip'
@@ -19,68 +31,68 @@ local lspkind = require 'lspkind'
 local tabnine = require('cmp_tabnine.config')
 
 tabnine:setup({
-	max_lines = 1000,
-	max_num_results = 20,
-	sort = true,
-	run_on_every_keystroke = true,
-	snippet_placeholder = '..',
-	ignored_file_types = {
-		-- default is not to ignore
-		-- uncomment to ignore in lua:
-		-- lua = true
-	},
-	show_prediction_strength = false
+    max_lines = 1000,
+    max_num_results = 20,
+    sort = true,
+    run_on_every_keystroke = true,
+    snippet_placeholder = '..',
+    ignored_file_types = {
+        -- default is not to ignore
+        -- uncomment to ignore in lua:
+        -- lua = true
+    },
+    show_prediction_strength = false
 })
 
 local source_mapping = {
-	buffer = "[Buffer]",
-	nvim_lsp = "[LSP]",
-	path = "[Path]",
+    buffer = "[Buffer]",
+    nvim_lsp = "[LSP]",
+    path = "[Path]",
     cmp_tabnine = "[TN]",
 }
 
 -- nvim-cmp setup
 local cmp = require 'cmp'
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-j>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
     },
-  }),
-  sources = {
-    { name = 'cmp_tabnine' },
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-	formatting = {
-		format = function(entry, vim_item)
-			-- if you have lspkind installed, you can use it like
-			-- in the following line:
-	 		vim_item.kind = lspkind.symbolic(vim_item.kind, {mode = "symbol"})
-	 		vim_item.menu = source_mapping[entry.source.name]
-	 		if entry.source.name == "cmp_tabnine" then
-	 			local detail = (entry.completion_item.data or {}).detail
-	 			vim_item.kind = ""
-	 			if detail and detail:find('.*%%.*') then
-	 				vim_item.kind = vim_item.kind .. ' ' .. detail
-	 			end
+    mapping = cmp.mapping.preset.insert({
+        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-j>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+    }),
+    sources = {
+        { name = 'cmp_tabnine' },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+    },
+    formatting = {
+        format = function(entry, vim_item)
+            -- if you have lspkind installed, you can use it like
+            -- in the following line:
+            vim_item.kind = lspkind.symbolic(vim_item.kind, {mode = "symbol"})
+            vim_item.menu = source_mapping[entry.source.name]
+            if entry.source.name == "cmp_tabnine" then
+                local detail = (entry.completion_item.data or {}).detail
+                vim_item.kind = ""
+                if detail and detail:find('.*%%.*') then
+                    vim_item.kind = vim_item.kind .. ' ' .. detail
+                end
 
-	 			if (entry.completion_item.data or {}).multiline then
-	 				vim_item.kind = vim_item.kind .. ' ' .. '[ML]'
-	 			end
-	 		end
-	 		local maxwidth = 80
-	 		vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth)
-	 		return vim_item
-	  end,
-	},
+                if (entry.completion_item.data or {}).multiline then
+                    vim_item.kind = vim_item.kind .. ' ' .. '[ML]'
+                end
+            end
+            local maxwidth = 80
+            vim_item.abbr = string.sub(vim_item.abbr, 1, maxwidth)
+            return vim_item
+        end,
+    },
 } 
