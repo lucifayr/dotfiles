267bfc0acb176bd4ee4c50b07676968219fd531f
jackboxx <lucygschwantner@gmail.com>
2023-03-05 17:08:03 +0100

    wip java lsp

---
+++ nvim/after/plugin/java.lua
@@ -0,0 +1,75 @@
+-- WIP
+--
+-- local jdtls_dir = os.getenv("HOME") .. 'mason/data/packages/jdtls'
+-- local config_dir = jdtls_dir .. 'config_linux'
+-- local plugins_dir = jdtls_dir .. 'plugins'
+-- local path_to_jar = plugins_dir .. 'org.eclipse.equinox.launcher.gtk.linux.x86_64_1.2.700.v20221108-1024.jar'
+
+-- local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+-- local root_dir = require("jdtls.setup").find_root(root_markers)
+-- if root_dir == "" then
+--     return
+-- end
+
+-- local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+-- local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
+-- os.execute("mkdir " .. workspace_dir)
+
+-- local config = {
+--     cmd = {
+--         'java',
+--         '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+--         '-Dosgi.bundles.defaultStartLevel=4',
+--         '-Declipse.product=org.eclipse.jdt.ls.core.product',
+--         '-Dlog.protocol=true',
+--         '-Dlog.level=ALL',
+--         '-Xms1g',
+--         '--add-modules=ALL-SYSTEM',
+--         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+--         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+--         '-jar', path_to_jar,
+--         '-configuration', config_dir,
+--         '-data', workspace_dir,
+--     },
+--     root_dir = root_dir,
+--     settings = {
+--         java = {
+--             home = 'wip',
+--             eclipse = {
+--                 downloadSources = true,
+--             },
+--             configuration = {
+--                 updateBuildConfiguration = "interactive",
+--                 runtimes = {
+--                     {
+--                         name = "JavaSE-18",
+--                         path = "/Users/ivanermolaev/Library/Java/JavaVirtualMachines/temurin-18.0.1/Contents/Home",
+--                     },
+--                 }
+--             },
+--             maven = {
+--                 downloadSources = true,
+--             },
+--             implementationsCodeLens = {
+--                 enabled = true,
+--             },
+--             referencesCodeLens = {
+--                 enabled = true,
+--             },
+--             references = {
+--                 includeDecompiledSources = true,
+--             },
+--             format = {
+--                 enabled = true,
+--                 settings = {
+--                     url = vim.fn.stdpath "config" .. "/lang-servers/intellij-java-google-style.xml",
+--                     profile = "GoogleStyle",
+--                 },
+--             }
+--         }
+--     },
+--     init_options = {
+--         bundles = {}
+--     },
+-- }
+-- require('jdtls').start_or_attach(config)
