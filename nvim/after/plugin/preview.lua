db331b9aa26ac5ffd2c27a2a41fe2ba57ebc30f7
Jackboxx <gschwantnermoritz@gmail.com>
2023-03-09 08:19:44 +0100

    update preview flags

--- nvim/after/plugin/preview.lua
+++ nvim/after/plugin/preview.lua
@@ -1,11 +1,12 @@
 vim.g.knap_settings = {
     textoutputtext = 'pdf',
     mdoutputext = 'pdf',
-    mdtopdf = "pandoc --from=gfm %docroot% -o %outputfile%",
+    mdtopdf =
+    "pandoc --from=gfm %docroot% -V 'monofont:JetBrains Mono' -V 'monofont:JetBrains Mono' -V 'geometry:margin=2cm' -o %outputfile%",
     mdtopdfviewerlaunch = "sioyek %outputfile%",
     textopdf = "pdflatex -interaction=batchmode -halt-on-error %docroot%",
 }
 
-vim.keymap.set({ 'n' },'<leader>pm', function() require("knap").process_once() end)
-vim.keymap.set({ 'n' },'<leader>pk', function() require("knap").close_viewer() end)
-vim.keymap.set({ 'n' },'<leader>pa', function() require("knap").toggle_autopreviewing() end)
+vim.keymap.set({ 'n' }, '<leader>pm', function() require("knap").process_once() end)
+vim.keymap.set({ 'n' }, '<leader>pk', function() require("knap").close_viewer() end)
+vim.keymap.set({ 'n' }, '<leader>pa', function() require("knap").toggle_autopreviewing() end)
