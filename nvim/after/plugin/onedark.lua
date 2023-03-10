22a1dab77c6c7996cede3e37a4daddffe03f8a9f
jackboxx <lucygschwantner@gmail.com>
2023-03-10 22:08:53 +0100

    Change color theme

--- nvim/after/plugin/onedark.lua
+++ nvim/after/plugin/onedark.lua
@@ -1,17 +1,22 @@
 require('onedark').setup {
-  style = "deep",
-  colors = {
-      red = '#88fcf8',
-  },
-  highlights = {
-    TSKeyword = {fg = '#31eae4', fmt =  'italic' },
-    TSKeywordFunction = {fg = '#31eae4', fmt =  'italic' },
-    TSKeywordOperator =  {fg = '#31eae4', fmt =  'italic' },
-    TSInclude =  {fg = '#31eae4', fmt =  'bold' },
-    TSVariable = {fg = '#e3eadc'},
-    TSVariableBuiltin = {fg = '#e3eadc', fmt = 'underline'},
-    TSComment = {fg = '#4d4d4f'},
-  }
+    style = "deep",
+    colors = {
+        fg = '#d4e0f5',
+        red = '#88fcf8',
+        blue = '#f5663c',
+        yellow = '#f2c757',
+        orange = '#f58e4c',
+        black = '#0c0e15',
+    },
+    highlights = {
+        TSKeyword = { fg = '#31eae4', fmt = 'italic' },
+        TSKeywordFunction = { fg = '#31eae4', fmt = 'italic' },
+        TSKeywordOperator = { fg = '#31eae4', fmt = 'italic' },
+        TSInclude = { fg = '#31eae4', fmt = 'bold' },
+        TSVariable = { fg = '#e3eadc' },
+        TSVariableBuiltin = { fg = '#e3eadc', fmt = 'underline' },
+        TSComment = { fg = '#4d4d4f' },
+    }
 }
 
 vim.g.background = dark
