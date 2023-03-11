2fd3e041d6c0d73d1760abc0d06671dfe9dc23f1
jackboxx <lucygschwantner@gmail.com>
2023-03-11 12:49:52 +0100

    Remove unneeded declrations

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,20 +1,11 @@
-require'lspconfig'.pyright.setup{}
-
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     highlight = {
         enable = true
     },
-  }
-
-require'lspconfig'.tsserver.setup{
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  root_dir = function() return vim.loop.cwd() end      -- run lsp for javascript in any directory
-
 }
 
-local on_attach = function(client, bufnr)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+local on_attach = function(client)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 end
