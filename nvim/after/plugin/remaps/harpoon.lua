b6d6da65371e97c500bf83c0dcabd8da201b14d7
jackboxx <lucygschwantner@gmail.com>
2023-03-15 20:26:18 +0100

    Add harpon file nav keybinds

--- nvim/after/plugin/remaps/harpoon.lua
+++ nvim/after/plugin/remaps/harpoon.lua
@@ -2,3 +2,8 @@ vim.keymap.set('n', '<leader>ja', '<cmd>lua require("harpoon.mark").add_file()<c
 vim.keymap.set('n', '<leader>js', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>')
 vim.keymap.set('n', '<leader>jk', '<cmd>lua require("harpoon.ui").nav_next()<cr>')
 vim.keymap.set('n', '<leader>jj', '<cmd>lua require("harpoon.ui").nav_prev()<cr>')
+
+vim.keymap.set('n', '<leader>1', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>')
+vim.keymap.set('n', '<leader>2', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>')
+vim.keymap.set('n', '<leader>3', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>')
+vim.keymap.set('n', '<leader>4', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>')
