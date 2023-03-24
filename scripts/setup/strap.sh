466f6dfbb585c466b6850f698b11224daa32fe5a
Jackboxx <gschwantnermoritz@gmail.com>
2023-03-24 07:52:26 +0100

    add ansible to base dependencies

--- scripts/setup/strap.sh
+++ scripts/setup/strap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pacstrap /mnt base linux linux-firmware grub efibootmgr man-db base-devel openssh unzip sudo networkmanager networkmanager-openvpn uthash git github-cli fzf neovim zsh curl
+pacstrap /mnt base linux linux-firmware grub efibootmgr ansible man-db base-devel openssh unzip sudo networkmanager networkmanager-openvpn uthash git github-cli fzf neovim zsh curl
 
 echo "Don't forget to install the needed GPU drivers"
