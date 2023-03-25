1930d6e7254136aaf884fbcc1331a3fc74fe3d82
jackboxx <lucygschwantner@gmail.com>
2023-03-25 08:45:05 +0100

    finish initial ansible migration

--- scripts/setup/keyboard.sh
+++
@@ -1,6 +0,0 @@
-sudo touch /etc/udev/rules.d/50-zsa.rules
-
-sudo groupadd plugdev
-sudo usermod -aG plugdev $USER
-
-echo "paste the config from https://github.com/zsa/wally/wiki/Linux-install#2-create-a-udev-rule-file into /etc/udev/rules.d/50-zsa.rules"
