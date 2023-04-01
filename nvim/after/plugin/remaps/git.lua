5b2adf93387b1a24ec38999685091c320910fc92
jackboxx <lucygschwantner@gmail.com>
2023-04-01 12:17:02 +0200

    wip add merge tool

--- nvim/after/plugin/remaps/git.lua
+++ nvim/after/plugin/remaps/git.lua
@@ -1,1 +1,5 @@
-vim.api.nvim_set_keymap('n', '<leader>gui', '<cmd>:GitUi<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gui', '<cmd>:GitUi<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>gd', '<cmd>Gvdiffsplit!<CR>')
+vim.keymap.set('n', '<leader>ff', '<cmd>diffget //2<CR>')
+vim.keymap.set('n', '<leader>jj', '<cmd>diffget //3<CR>')
