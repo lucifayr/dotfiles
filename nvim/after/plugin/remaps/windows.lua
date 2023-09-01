e5e1c76d8ec391d06384409219b16975c01b119b
jackboxx <lucygschwantner@gmail.com>
2023-09-01 18:28:24 +0200

    add close all non active windows command

--- nvim/after/plugin/remaps/windows.lua
+++ nvim/after/plugin/remaps/windows.lua
@@ -9,3 +9,17 @@ vim.keymap.set("n", "<C-H>", "<C-W>h")
 vim.keymap.set("n", "<C-J>", "<C-W>j")
 vim.keymap.set("n", "<C-K>", "<C-W>k")
 vim.keymap.set("n", "<C-L>", "<C-W>l")
+
+-- close all
+function close_non_active_windows()
+    local current_win = vim.api.nvim_get_current_win()
+    local wins_in_tab = vim.api.nvim_tabpage_list_wins(0)
+
+    for _, handle in pairs(wins_in_tab) do
+        if handle ~= current_win then
+            vim.api.nvim_win_close(handle, false)
+        end
+    end
+end
+
+vim.keymap.set("n", "<leader>wk", "<cmd>lua close_non_active_windows()<cr>")
