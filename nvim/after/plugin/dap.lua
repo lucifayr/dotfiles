e21613b55e8c8a265ada73da55de18389ba4ff8f
jackboxx <lucygschwantner@gmail.com>
2023-09-02 18:30:43 +0200

    switch to codelldb for rust debugging

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -7,38 +7,25 @@ dapui.setup()
 
 -- setup adapters
 
-dap.adapters.lldb = {
-    type = 'executable',
-    command = '/usr/bin/lldb-vscode',
-    name = 'lldb'
+dap.adapters.codelldb = {
+    type = 'server',
+    port = '${port}',
+    executable = {
+        command = home .. '/mason/data/bin/codelldb',
+        args = { '--port', '${port}' }
+    }
 }
 
 -- setup language config
 
-dap.configurations.cpp = {
-    {
-        name = 'Launch C++ debugger',
-        type = 'lldb',
-        request = 'launch',
-        program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        args = {},
-    },
-}
-
-dap.configurations.c = dap.configurations.cpp
-
 dap.configurations.rust = {
     {
         name = 'Launch Rust debugger',
-        type = 'lldb',
+        type = 'codelldb',
         request = 'launch',
         program = function()
             local pkg_name = vim.fn.trim(vim.fn.system(
-                'cat Cargo.toml | grep "name" | awk -F\\" \'{print $2}\''))
+                'cat Cargo.toml | grep "name" | awk -F\\" \'{print $2}\' | head -n 1'))
             local default_location = vim.fn.getcwd() .. '/target/debug/' .. pkg_name
 
             return vim.fn.input('Path to executable: ', default_location)
@@ -48,26 +35,6 @@ dap.configurations.rust = {
         end,
         cwd = '${workspaceFolder}',
         stopOnEntry = false,
-        args = {},
-        initCommands = function()
-            -- Find out where to look for the pretty printer Python module
-            local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
-
-            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
-            local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
-
-            local commands = {}
-            local file = io.open(commands_file, 'r')
-            if file then
-                for line in file:lines() do
-                    table.insert(commands, line)
-                end
-                file:close()
-            end
-            table.insert(commands, 1, script_import)
-
-            return commands
-        end,
     },
 }
 -- UI hooks
--- nvim/after/plugin/remaps/dap.lua
+++ nvim/after/plugin/remaps/dap.lua
@@ -2,14 +2,16 @@ local dap = require('dap')
 local dapui = require("dapui")
 
 
+vim.keymap.set('n', '<leader>dr', function() dap.run_to_cursor() end)
 vim.keymap.set('n', '<leader>dc', function() dap.continue() end)
 vim.keymap.set('n', '<leader>dso', function() dap.step_over() end)
 vim.keymap.set('n', '<leader>dsi', function() dap.step_into() end)
-vim.keymap.set('n', '<leader>dsot', function() dap.step_out() end)
+vim.keymap.set('n', '<leader>dst', function() dap.step_out() end)
 vim.keymap.set('n', '<leader>db', function() dap.toggle_breakpoint() end)
 vim.keymap.set('n', '<leader>dlb',
     function() dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
 
-vim.keymap.set('n', '<leader>dr', function() dap.run() end)
+vim.keymap.set('n', '<leader>dd', function() dapui.disconnect() end)
+vim.keymap.set('n', '<leader>dk', function() dapui.terminate() end)
 
 vim.keymap.set('n', '<leader>dui', function() dapui.toggle() end)
