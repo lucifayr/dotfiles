0f1679dd08458820e022424cd7582f2c864148d9
Jackboxx <gschwantnermoritz@gmail.com>
2023-09-30 14:11:44 +0200

    reduce amount of notifications

--- nvim/after/plugin/notifications.lua
+++ nvim/after/plugin/notifications.lua
@@ -21,4 +21,18 @@ require("noice").setup({
         inc_rename = false,           -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
+
+    routes = {
+        {
+            filter = {
+                event = "msg_show",
+                kind = "",
+            },
+            opts = { skip = true },
+        },
+        {
+            view = "notify",
+            filter = { event = "msg_showmode" },
+        },
+    },
 })
