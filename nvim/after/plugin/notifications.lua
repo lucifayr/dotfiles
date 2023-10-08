7d5167da2f2a443f8f9a528eb58ac5f5455b1fdc
jackboxx <lucygschwantner@gmail.com>
2023-10-08 12:53:30 +0200

    filter notifications more granularly

--- nvim/after/plugin/notifications.lua
+++ nvim/after/plugin/notifications.lua
@@ -4,6 +4,40 @@ require("notify").setup({
     background_colour = "#000000",
 })
 
+local filter_patterns = {
+    " fewer lines",
+    " line less",
+    " lines less",
+    " lines yanked",
+    " more line",
+    "Pattern not found",
+    "change; after",
+    "change; before",
+    "changes; after",
+    "changes; before",
+    "search hit ",
+    "written",
+}
+
+local filters = {}
+
+for _, pattern in ipairs(filter_patterns) do
+    table.insert(filters, {
+        filter = {
+            event = "msg_show",
+            kind = "",
+            find = pattern,
+        },
+        opts = { skip = true },
+    })
+end
+
+table.insert(filters,
+    {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+    })
+
 require("noice").setup({
     lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -22,17 +56,5 @@ require("noice").setup({
         lsp_doc_border = false,       -- add a border to hover docs and signature help
     },
 
-    routes = {
-        {
-            filter = {
-                event = "msg_show",
-                kind = "",
-            },
-            opts = { skip = true },
-        },
-        {
-            view = "notify",
-            filter = { event = "msg_showmode" },
-        },
-    },
+    routes = filters,
 })
