0cb3e4c616f070310aea301468e86e53caaf1777
jackboxx <lucygschwantner@gmail.com>
2023-10-18 21:21:43 +0200

    update archwiki-rs keymaps to open man page of wiki entry

--- nvim/after/plugin/remaps/archwiki.lua
+++ nvim/after/plugin/remaps/archwiki.lua
@@ -1,6 +1,7 @@
-vim.keymap.set('n', '<leader>awp', '<cmd>lua require("archwiki.api").select_page_from_local()<cr><cmd>:set nospell<cr>')
-vim.keymap.set('n', '<leader>awm',
-    '<cmd>lua require("archwiki.api").select_page_from_local("-f markdown")<cr><cmd>:set nospell<cr>')
-vim.keymap.set('n', '<leader>awh',
-    '<cmd>lua require("archwiki.api").select_page_from_local("-f html")<cr><cmd>:set nospell<cr>')
-vim.keymap.set('n', '<leader>awu', '<cmd>lua require("archwiki.api").update_category_from_local()<cr>')
+local function open_page_as_man(page)
+    vim.cmd("!tmux neww ~/projects/dotfiles/custom/archwiki-man " .. page)
+end
+
+vim.keymap.set('n', '<leader>aw', function()
+    local page = require("archwiki.api").select_page_from_local(nil, nil, open_page_as_man)
+end)
